cfdeb949151ab580a2e9e50bd6b4d5c2363ae618
gabriel-q88 <40808801+gabriel-q88@users.noreply.github.com>
2018-07-09 05:32:06 +0200

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -4,8 +4,17 @@ function mostrar()
 var edad;
 edad = document.getElementById("edad").value;
 edad = parseInt(edad);
-if()
+if(edad >= 18)
+{alert ("la Persona es mayor de edad")}
 
-
-
+ else { 
+     if (edad <=17 && edad >=13)
+      {alert("la persona es adolecente") }
+              
+     if (edad <13)         
+       alert("la persona es menor de edad")}
+                               
+         
+                    
+                  
 }//FIN DE LA FUNCIÓN
