04e0e4543636dc30fa586658c08078c965c21a00
gabriel-q88 <40808801+gabriel-q88@users.noreply.github.com>
2018-07-25 22:11:48 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -2,9 +2,17 @@ function mostrar()
 {
 
 	var contador=0;
+	var numero;
 	var acumulador=0;
 	var respuesta='si';
-
+   do {
+	   numero = parseInt (prompt("Ingrese un numero: ")); 
+   acumulador = acumulador + numero;
+	 contador = contador + 1;
+	respuesta = prompt("Desea reingresar numero?: ");
+	respuesta = respuesta.toLowerCase();
+   }
+    while (respuesta == "si");
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,14 +1,30 @@
 function mostrar()
-{
-
-	var contador=0;
+{   var contador=0;
+	var numero;
+	var respuesta='si';  // <---plantea las variables
 	var positivo=0;
 	var negativo=1;
-	
-	var respuesta='si';
 
+   do { //<--- hacer...
+	   numero = parseInt (prompt("Ingrese un numero: "));
+	   if (numero >=0 ) { // <--- si la variable "numero" es mayor o igual a "0"
+         positivo = positivo + numero;// <---ejecuto la suma de la variable de los positivos 
+    
+	  }
+	  else { //<--- ademas...
+      negativo = negativo * numero;//<---- ejecuto la multiplicacion de la variable
+	 contador++;//<---- es lo mismo que decir "contador = contador + 1"
+	 }
 
+	  
+	respuesta = prompt("Desea reingresar numero?: ");
+	respuesta = respuesta.toLowerCase();
+      }
+      while (respuesta == "si");
+   
 document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
+if (contador == 0)
+	{negativo = 0;}
 
+document.getElementById('producto').value=negativo;
 }//FIN DE LA FUNCIÓN
